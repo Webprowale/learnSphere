18fbe35748cc2faa5f996f5b181d589e42b4bef4
Webprowale <webprowale@gmail.com>
2024-08-21 01:01:22 -1100

    course end created

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,7 +10,16 @@ $routes->get('/register', 'AuthController::index');
 $routes->post('/register', 'AuthController::register');
 $routes->get('/login', 'AuthController::login');
 $routes->post('/login', 'AuthController::confirm_login');
+
 $routes->group('/dashboard', ['filter' => 'auth'], function ($routes) {
     $routes->get('', 'UserController::index');
     $routes->get('logout', 'AuthController::logout');
+});
+
+$routes->group('/tutor',function($routes){
+     $routes->get('', 'CourseController::index');
+     $routes->post('/create-course', 'CourseController::create_course');
+     $routes->post('/create-lesson', 'CourseController::create_lesson');
+     $routes->post('/create-quiz', 'CourseController::create_quiz');
+     
 });
